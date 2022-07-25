ebbb3ece6d8eed9efb9e727751ec1db1cfe73f71
danielronalds <nzdanron@gmail.com>
2022-07-25 22:41:10 +1200

    It has a game loop now!

--- src/main.rs
+++ src/main.rs
@@ -10,37 +10,42 @@ fn main() {
                                                                // upper bounds
     
     println!("The secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
+     
+         let mut guess = String::new();
+     
+         io::stdin()
+             .read_line(&mut guess)// The & tells the compiler its a refrence, 
+                                   // meaning it doesn't have to copy the data
+                                   // into memory multiple times. like var's
+                                   // refrences are immutable by default.
+             .expect("Failed to read line"); // Most basic error handling.
+                                             // It's considered good practive 
+                                             // to use a whitespace or newline
+                                             // when you call a method with 
+                                             // .method_name()
+         // This is known as shadowing
+         // Despite already having a var named guess, rust lets us reuse the 
+         // name shadowing the previous value of guess with a new one. This is 
+         // instead of using two different vars
+         let guess: u32 = guess.trim().parse().expect("Please type a number!");
+         // trim() on a string removes whitespace and \n at the beginning & end
+         // : after a var name tells rust we'll annotate the var's type.
+         // parse(), like read_line(), returns a Result type, so we'll handle
+         // it with .expect()
+     
+         println!("You guessed: {guess}");
+     
+         // match experions are made out of arms, this one has 3
+         match guess.cmp(&secret_number) {
+             Ordering::Less => println!("Too small!"), // Refered to as an arm
+             Ordering::Greater => println!("Too big!"),
+             Ordering::Equal => {
+                 println!("You win!");
+                 break;
+             }
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)// The & tells the compiler its a refrence, 
-                              // meaning it doesn't have to copy the data
-                              // into memory multiple times. like var's
-                              // refrences are immutable by default.
-        .expect("Failed to read line"); // Most basic error handling.
-                                        // It's considered good practive to
-                                        // use a whitespace or newline when
-                                        // you call a method with 
-                                        // .method_name()
-    // This is known as shadowing
-    // Despite already having a var named guess, rust lets us reuse the name
-    // shadowing the previous value of guess with a new one. This is instead
-    // of using two different vars
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    // trim() on a string removes whitespace and \n at the beginning and end
-    // : after a var name tells rust we'll annotate the var's type.
-    // parse(), like read_line(), returns a Result type, so we'll handle it
-    // with .expect()
-
-    println!("You guessed: {guess}");
-
-    // match experions are made out of arms, this one has 3
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"), // Refered to as an arm
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+         }
     }
 }
