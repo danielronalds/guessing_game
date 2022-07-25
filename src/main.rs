b0052101013e3ace3e49b9007f1857f62d43d961
danielronalds <nzdanron@gmail.com>
2022-07-25 21:59:17 +1200

    Finished the first part, user can enter a number as a guess and it writes that number to a new line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)// The & tells the compiler its a refrence, 
+                              // meaning it doesn't have to copy the data
+                              // into memory multiple times. like var's
+                              // refrences are immutable by default.
+        .expect("Failed to read line"); // Most basic error handling.
+                                        // It's considered good practive to
+                                        // use a whitespace or newline when
+                                        // you call a method with 
+                                        // .method_name()
+
+    println!("You guessed: {guess}");
 }
