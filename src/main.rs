c04e7f5e9a575edd50cceef95395fe9764b51143
danielronalds <nzdanron@gmail.com>
2022-07-25 22:35:33 +1200

    A secret number is now generated and the user can guess it's value, with feedback if it's higher than or lower than the secret number, or if it is the secret number. The user currently only get's one guess

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+   
+    let secret_number = rand::thread_rng().gen_range(1..=100); // inclusive on
+                                                               // both lower &
+                                                               // upper bounds
     
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -17,6 +25,22 @@ fn main() {
                                         // use a whitespace or newline when
                                         // you call a method with 
                                         // .method_name()
+    // This is known as shadowing
+    // Despite already having a var named guess, rust lets us reuse the name
+    // shadowing the previous value of guess with a new one. This is instead
+    // of using two different vars
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    // trim() on a string removes whitespace and \n at the beginning and end
+    // : after a var name tells rust we'll annotate the var's type.
+    // parse(), like read_line(), returns a Result type, so we'll handle it
+    // with .expect()
 
     println!("You guessed: {guess}");
+
+    // match experions are made out of arms, this one has 3
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"), // Refered to as an arm
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
